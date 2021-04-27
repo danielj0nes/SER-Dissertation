1d6ba5ecccdd05d5ac7f39ffa345d3f86358836f
danielj0nes <>
2021-04-27 10:40:18 +0100

    Completed GUI with load/record audio direct functionality

--- SER_Application.py
+++ SER_Application.py
@@ -1,29 +1,21 @@
 """Daniel Jones - SER GUI"""
 # Import libraries
-import os
-import numpy as np
-import pandas as pd
-import glob
-import sounddevice as sd
-import tkinter as tk
-from SER_helpers import convert_audio, labels
-from keras.models import load_model
-from keras import optimizers
-from keras.utils import np_utils
-from tkinter import *
+from tkinter import LabelFrame, StringVar, Label, Button, END, Text
 from tkinter.filedialog import askopenfilename
 import _thread
+import tkinter as tk
+import numpy as np
+
+from keras.models import load_model
+from keras import optimizers
+from Voice_Recorder import RecAUD
+from SER_helpers import convert_audio, labels
 
 # Load model with standard configuration settings
 opt = optimizers.Adam(learning_rate=0.0001, beta_1=0.9,  beta_2=0.999, amsgrad=False)
 model = load_model("model/SER_Model_2DCNN.h5")
 model.load_weights("model/SER_Model_2DCNN.h5")
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
-
-# Constant variables
-FS = 44100
-sd.default.samplerate = FS
-sd.default.channels = 2
 
 def predict_emotion(filepath):
     """Convert the audio and use the model to make a prediction"""
@@ -36,17 +28,26 @@ def predict_emotion(filepath):
     for key, value in sorted(converted.items(), key=lambda item: item[1], reverse=True):
         values_box.insert(END, key + " " + value + "\n")
 
-def record():
-    activity_var.set("Recording...")
-
 def load_audio():
     """Open a file to get the file path then predict the emotion"""
     filepath = askopenfilename()
     file_var.set(filepath)
     _thread.start_new_thread(predict_emotion, (filepath,))
-    
+
+def record():
+    """Activate the helper function to record some audio, predict emotions once finished"""
+    activity_var.set("Recording...")
+    predict_emotion(ra.start_record())
+
+def stop_record():
+    """End the recording and set the file label to the newly recorded file"""
+    activity_var.set("Not recording")
+    file_var.set(ra.last_name)
+    ra.stop()
+
 # Initialisations
 app = tk.Tk()
+ra = RecAUD(app)
 wrapper = LabelFrame(app, text="Speech Emotion Recogniser")
 wrapper.pack(fill="both", expand="yes", padx=10, pady=10)
 # Set labels
@@ -54,7 +55,7 @@ file_var = StringVar()
 activity_var = StringVar()
 values_var = StringVar()
 file_var.set("No file imported")
-activity_var.set("No activity detected")
+activity_var.set("Not recording")
 values_var.set("Predictions (in order of most likely)")
 file_lbl = Label(wrapper, textvariable=file_var)
 activity_lbl = Label(wrapper, textvariable=activity_var)
@@ -62,17 +63,18 @@ values_lbl = Label(wrapper, textvariable=values_var)
 # Set buttons
 file_btn = Button(wrapper, text="Load audio", command=load_audio)
 activity_btn = Button(wrapper, text="Start recording", command=record)
+stop_btn = Button(wrapper, text="Stop recording", command=stop_record)
 # Label positioning
 file_lbl.place(x=0, y=0)
 activity_lbl.place(x=0, y=130)
+values_lbl.place(x=285, y=20)
 # Button positioning
 file_btn.place(x=0, y=30)
 activity_btn.place(x=0, y=160)
+stop_btn.place(x=100, y=160)
 # Value box positioning
-values_lbl.place(x=285, y=20)
 values_box = Text(app, width=40, height=12) # Height = length of predictions
 values_box.place(x=300, y=70)
-
 # GUI defaults
 app.title("SER - Daniel Jones")
 app.geometry("800x450")
---
+++ Voice_Recorder.py
@@ -0,0 +1,50 @@
+# Import the necessary modules.
+import tkinter
+import tkinter as tk
+import tkinter.messagebox
+import pyaudio
+import wave
+import os
+from datetime import datetime
+
+
+class RecAUD:
+
+    def __init__(self, main, chunk=3024, frmat=pyaudio.paInt16, channels=2, rate=44100, py=pyaudio.PyAudio()):
+
+        self.last_name = ""
+        self.main = main
+        self.collections = []
+        self.CHUNK = chunk
+        self.FORMAT = frmat
+        self.CHANNELS = channels
+        self.RATE = rate
+        self.p = py
+        self.frames = []
+        self.st = 1
+        self.stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
+
+    def start_record(self):
+        self.st = 1
+        self.frames = []
+        stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
+        print("Recording...")
+        filename = f"SER_Recordings/SER_Audio_{datetime.now().strftime('%H%M%S')}.wav"
+        self.last_name = filename
+        while self.st == 1:
+            data = stream.read(self.CHUNK)
+            self.frames.append(data)
+            self.main.update()
+        stream.close()
+        wf = wave.open(filename, "wb")
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
+        wf.setframerate(self.RATE)
+        wf.writeframes(b''.join(self.frames))
+        wf.close()
+        return os.path.abspath(filename)
+
+    def stop(self):
+        print("Finished recording")
+        self.st = 0
+
